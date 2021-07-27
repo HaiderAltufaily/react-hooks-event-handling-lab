b3da3045261214b900646d3be0d7183828014e6a
haider <haiderlek@gmail.com>
2021-07-27 15:54:21 +0300

    commit tree c3ca62686c1ac74a0e3c9d8e444a910a4d470d57

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+  function onFocus() {
+    return console.log("Good!");
+  }
+  function onBlur() {
+    return console.log("Hey! Eyes on me!");
+  }
+  return (
+    <div>
+      <button onFocus={onFocus} onBlur={onBlur}>
+        Eyes on me
+      </button>
+    </div>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
-// Code Keypad Component Here
+import React from "react";
+
+function Keypad() {
+  return (
+    <div>
+      <input
+        type="password"
+        name="password"
+        onChange={() => console.log("Entering password...")}
+      />
+    </div>
+  );
+}
+
+export default Keypad;
